4dda08762f42cedabd2c0a5efec77219b37368f7
Brad Dennis <BradDennis@my.unt.edu>
2020-08-25 14:20:01 -0500

    Working Elastic Search with Json Parser

--- ElasticSearch.py
+++ ElasticSearch.py
@@ -1,1 +1,31 @@
-print ("Hello")
+import requests
+import json
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+import os
+
+es = Elasticsearch()
+i=1
+file=open("docs.json")
+docs=json.loads(file.read())
+
+
+
+while i <= len(docs['articles']) :
+    res = es.index(index="articles", id=i, body=docs['articles'][i-1])
+    i=i+1
+
+
+
+"""
+res=es.get(index="articles",id=1)
+print(res)
+res=es.get(index="articles",id=2)
+print(res)
+res=es.get(index="articles",id=3)
+print(res)
+"""
+
+
+res= es.search(index='articles',body={'query':{'match':{'text':'shooting'}}})
+print (res['hits']['hits'])
