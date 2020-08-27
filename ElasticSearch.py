4b2789973639487c80bb3fc20cf024259a3dfaba
Brad Dennis <BradDennis@my.unt.edu>
2020-08-27 14:55:56 -0500

    Added newspaper for parsing articles and removed need for external json file

--- ElasticSearch.py
+++ ElasticSearch.py
@@ -3,29 +3,48 @@ import json
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 import os
+import newspaper
+from newspaper import Source, Article, fulltext
 
 es = Elasticsearch()
 i=1
-file=open("docs.json")
-docs=json.loads(file.read())
+
+data = {}
+data['articles'] = []
+
+cnn_paper = newspaper.build('http://cnn.com',language='en', fetch_images=False, number_threads=20)
+for article in (cnn_paper.articles):
+    if i<100:
+        try:
+            print(i)
+            print(article.url)
+            article.download()
+            print("article downloaded")
+            article.parse()
+            print("article parsed")
+            article.authors
+            article.text
+            article.title
+            data['articles'].append({
+                'title': article.title,
+                'author': article.authors,
+                'text': article.text
+            })
+            print("pushed into data")
+            res = es.index(index="articles", id=id, body=data['articles'][0])
+            print("indexed")
+            data['articles'].clear()
+            print("dict cleared")
+            i = i + 1
+        except:
+            i = i + 1
+            pass
 
 
 
-while i <= len(docs['articles']) :
-    res = es.index(index="articles", id=i, body=docs['articles'][i-1])
-    i=i+1
 
 
 
-"""
-res=es.get(index="articles",id=1)
-print(res)
-res=es.get(index="articles",id=2)
-print(res)
-res=es.get(index="articles",id=3)
-print(res)
-"""
-
-
-res= es.search(index='articles',body={'query':{'match':{'text':'shooting'}}})
+res= es.search(index='articles',body={'query':{'match':{'text':'virus'}}})
 print (res['hits']['hits'])
+
